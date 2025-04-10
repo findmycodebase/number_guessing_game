fb776976bec9214386a212f2472811a02e36f4cc
Oyebode Rejoice <oyeboderejoice@gmail.com>
2025-04-10 12:52:30 -0400

    Applied error validation for inouted values

--- calculator.py
+++ calculator.py
@@ -12,8 +12,18 @@ def division(n1,n2):
   return float(n1) / float(n2)
 
 if __name__ == "__main__":
-  n1 = input("Enter your first number ")
-n2 = input("Enter your second number ")
+  while True:
+    try:
+        n1 = float(input("Enter your first number: "))
+        break 
+    except ValueError:
+        print("Invalid input. Please enter a number.")
+  while True:
+    try:
+        n2 = float(input("Enter your second number: "))
+        break 
+    except ValueError:
+        print("Invalid input. Please enter a number.")
 operation = input("Enter the operation you want to perform (+, -, * and /) ")
 valid_operations = ["+", "-", "*", "/"]
 
